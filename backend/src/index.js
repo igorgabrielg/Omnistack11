5b522a32cf7057bc9c47c7687ff128db07b2e29d
Igor Gabriel <igor.gabrielg@gmail.com>
2020-03-26 14:34:30 -0300

    Utilização do Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const routes = require('./routes')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
+const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
- app.listen(3333)
+app.listen(3333);

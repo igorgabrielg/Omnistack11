1350f95894717f8bad79d37779cac32c291140b4
Igor Gabriel <igor.gabrielg@gmail.com>
2020-03-26 18:46:33 -0300

    Mobile da aplicação

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator()
+
+import Casos from './pages/Casos'
+import Detalhes from './pages/Detalhes'
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="Casos" component={Casos}/>
+                <AppStack.Screen name="Detalhes" component={Detalhes}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
